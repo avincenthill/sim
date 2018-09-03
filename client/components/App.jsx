68466cebd85a2017a0063116f8a92142e035b286
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-09-03 02:35:39 -0700

    Render prototype grid

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,10 +3,28 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.n = 100;
+    const row = new Array(this.n).fill(0);
+    const sim = new Array(this.n).fill(row);
+    this.state = {
+      sim: sim,
+    }
   }
   render() {
+    let sim = this.state.sim;
     return (
-      <h1>Hello World!</h1>
+      <React.Fragment>
+        <h1>Hello World!</h1>
+        <div className='board'>
+          {sim.map((row, i) => {
+            return row.map((cell, j) => {
+              return <div className='cell'>0</div>
+            })
+          })
+          }
+        </div>
+        <h2>Goodbye</h2>
+      </React.Fragment>
     )
   }
 }
